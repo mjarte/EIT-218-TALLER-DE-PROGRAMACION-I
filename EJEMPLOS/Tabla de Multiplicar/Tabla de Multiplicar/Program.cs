4cd31abd32ea787547349a7caf925220b3bddafa
mjarte <michell.jaureguiv@correoaiep.cl>
2018-07-19 12:30:28 -0400

    modificacion salto de linea
    modificacion salto de linea

--- EJEMPLOS/Tabla de Multiplicar/Tabla de Multiplicar/Program.cs
+++ EJEMPLOS/Tabla de Multiplicar/Tabla de Multiplicar/Program.cs
@@ -10,7 +10,7 @@ namespace Tabla_de_Multiplicar
     {
         static void Main(string[] args)
         {
-            Console.Write("Ingrese un numero: \n\n");
+            Console.Write("Ingrese un numero: \n\n\n");
             string numero = Console.ReadLine();
 
             int num1 = Convert.ToInt32(numero);
