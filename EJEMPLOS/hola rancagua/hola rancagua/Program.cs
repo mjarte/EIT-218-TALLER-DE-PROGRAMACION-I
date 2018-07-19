2c6e1eeb6d474fe6bdff602b0efee2e110697f9b
mjarte <michell.jaureguiv@correoaiep.cl>
2018-07-19 12:08:20 -0400

    Ejemplo_1
    Primer ejemplo

---
+++ EJEMPLOS/hola rancagua/hola rancagua/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_rancagua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese un numero: \n\n\n");
+            string numero = Console.ReadLine();
+
+            int num1 = Convert.ToInt32(numero);
+            int i = 1;
+            int sum = 0;
+
+            while(i<=10){
+                sum = num1 * i;
+
+                Console.WriteLine(num1 + " * " + i + " = " + sum);
+
+                i++;
+            }
+            
+
+        }
+    }
+}
