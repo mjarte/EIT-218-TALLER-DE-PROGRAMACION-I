ca2dd42a7f06da942e8f3bed1f7b816582936160
mjarte <michell.jaureguiv@correoaiep.cl>
2018-07-19 12:27:32 -0400

    Tabla de Multiplicar
    Ejemplo Tabla de Multiplicar

--- EJEMPLOS/Tabla de Multiplicar/Tabla de Multiplicar/Program.cs
+++ EJEMPLOS/Tabla de Multiplicar/Tabla de Multiplicar/Program.cs
@@ -4,29 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hola_rancagua
+namespace Tabla_de_Multiplicar
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Console.Write("Ingrese un numero: \n\n\n");
+            Console.Write("Ingrese un numero: \n\n");
             string numero = Console.ReadLine();
 
             int num1 = Convert.ToInt32(numero);
             int i = 1;
             int sum = 0;
 
-            while(i<=10){
+            while (i <= 10)
+            {
                 sum = num1 * i;
 
                 Console.WriteLine(num1 + " * " + i + " = " + sum);
 
                 i++;
             }
-            
-
         }
     }
 }
